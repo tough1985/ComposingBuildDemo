f3a43fbdf0c2e7a4a5e35db365417e8fb6cac4b5
刘坤 <liukun@fangdd.com>
2020-10-14 14:23:50 +0800

    [NFF] 添加inclueBuild

--- ComposingBuildApp/app/src/main/java/me/xiba/composingbuildapp/MainActivity.kt
+++ ComposingBuildApp/app/src/main/java/me/xiba/composingbuildapp/MainActivity.kt
@@ -2,10 +2,15 @@ package me.xiba.composingbuildapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import me.xiba.composinglibrary.Constant
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 引入ComposingBuildLibrary的Constant.TAG
+        findViewById<TextView>(R.id.tv_content).text = Constant.TAG
     }
 }
---
+++ ComposingBuildLibrary/composinglibrary/src/main/java/me/xiba/composinglibrary/Constant.kt
@@ -0,0 +1,12 @@
+package me.xiba.composinglibrary
+
+/**
+ * @Author:liukun
+ * @Date: 2020-10-14 14:17
+ * @Description:
+ */
+class Constant {
+    companion object{
+        const val TAG = "ComposingLibrary"
+    }
+}
