6dba0ed15f61fb58e526cbaa9c4aa6eba130afab
刘坤 <liukun@fangdd.com>
2020-10-15 17:20:32 +0800

    testcommit

---
+++ ComposingBuildLibrary/composinglibrary/gitcommit.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# 传递参数
+
+if [ ! $1 ]
+then
+    echo "########## 请输入提交信息 ##########"
+    exit 1;
+fi
+
+echo "########## 开始提交 ##########"
+
+echo "commitMessage: $1"
+
+git add -A
+git status
+
+echo $?
+
+#echo "########## 请输入提交信息 ##########"
+
+git commit -m "$1"
+
+if [ $? -ne 0 ]
+then
+  echo "git commit 错误"
+  exit 1
+fi
+
+git fetch
+if [ $? -ne 0 ]
+then
+  echo "git fetch 错误"
+  exit 1
+fi
+
+git rebase
+if [ $? -ne 0 ]
+then
+  echo "git rebase 错误"
+  exit 1
+fi
+
+git push -u origin
+if [ $? -ne 0 ]
+then
+  echo "git push 错误"
+  exit 1
+fi
+
+echo "########## 提交结束 ##########"
